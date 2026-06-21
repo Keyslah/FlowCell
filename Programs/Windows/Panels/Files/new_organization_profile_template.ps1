ed0063c3abbdf7f09a12a300a984f4e39b0cb5be
Keyslah <aek290@gmail.com>
2026-06-21 16:59:50 -0500

    Remove extra profile template panel button

--- Programs/Windows/Panels/Files/new_organization_profile_template.ps1
+++
@@ -1,5 +0,0 @@
-# Description: New Organization Profile Template.
-# Copy this file, rename it, and set the profile id below.
-$ProfileId = ''
-& (Join-Path $PSScriptRoot 'new_organization.ps1') -ProfileId $ProfileId
-exit $LASTEXITCODE
